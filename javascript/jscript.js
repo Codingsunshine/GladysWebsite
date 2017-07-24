1bd591a6a10131644f68952857c28f205a2dc55b
Codingsunshine <Jessica.baines@gmail.com>
2017-07-24 06:59:15 -0500

    Initial Class Project - Gladys - Uploading draft files
    Gladys Website Uploading Working Files

---
+++ javascript/jscript.js
@@ -0,0 +1,19 @@
+/*Associated JavaScript Top Nav***********************
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}*/
+/*--JScript for map section
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}*/
